411d53db40babb87b32b97da4fdf19e9fb6ae483
guan_manjaro <1057368050@qq.com>
2022-11-05 20:31:20 +0800

    add record

--- data/wiki/a.cpp
+++ data/wiki/a.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
+int fun1(){
+    // transform link.tsv to binary file
     string src,tar;
     int tot = 1;
     freopen("./links.tsv","r",stdin);
@@ -28,4 +29,32 @@ int main(){
         fwrite(&v, sizeof(v), 1, f_wtr); 
     } 
     cerr<<mp.size()<<endl;
+    return 0;
+}
+
+int fun2(){
+    //transform record.txt to binary file
+    int src,tar;
+    int tot = 1;
+    freopen("./record.txt","r",stdin);
+    map<string,int> mp;
+    ofstream out("wiki.bi", ios::out | ios::binary);
+    FILE *f_wtr = NULL;
+    if((f_wtr = fopen("wiki.bi","wb")) == NULL)  {// w for write, b for binary
+        cerr<<"open f_wtr file failed!"<<endl;
+        return -1;
+    }
+    while(cin>>src>>tar){
+        int u,v;
+        u = src;
+        v = tar;
+        fwrite(&u, sizeof(u), 1, f_wtr); 
+        fwrite(&v, sizeof(v), 1, f_wtr); 
+    } 
+    // cerr<<mp.size()<<endl;
+    return 0;
+}
+
+int main(){
+    fun2();
 }

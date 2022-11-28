876f821dcf4dfacb9c6d7f0cad2a12564d088649
guan_manjaro <1057368050@qq.com>
2022-11-28 16:38:22 +0800

    add cpp kcore and speed opt

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -26,7 +26,8 @@ public:
     priority_queue<P,vector<P>,cmp> heap;
     void add(int rank,int id){
         if(check.find(id) != check.end()) return;
-        if(check.size() == maxi_size && heap.top().first<rank){
+        if(check.size() == maxi_size && heap.top().first >= rank) return;
+        if(check.size() == maxi_size && heap.top().first < rank){
             this->pop();
         }
         heap.push(P(rank,id));
@@ -53,16 +54,105 @@ public:
         assert(heap.size() != 0);
         return heap.top().second;
     }
+    void clear(){
+        check.clear();
+        heap = priority_queue<P,vector<P>,cmp>();
+    }
 };
 
-// class K_core{
-//     void get_kcore(){
-//         vector<P> mp_node;
-//         vector<>
-//     }
-// };
 HyperNode *Node;
 HyperEdge *Edge;
+class K_core{
+    int node_num,edge_num;
+    map<int,int> n_deg,e_deg;
+    HyperNode *Node;
+    HyperEdge *Edge;
+    
+public:
+    K_core(HyperNode *node,HyperEdge *edge,int n,int m){
+         node_num = n;
+         edge_num = m;
+         Node = node;
+         Edge = edge;
+         for(int i=0;i<node_num;i++) n_deg[i] = Node[i].degree;
+         for(int i=0;i<edge_num;i++) e_deg[i] = Edge[i].degree;
+    }
+    vector<int> get_kcore(){
+        vector<int> ret;
+        map<int,int> mp_node,mp_edge;
+        vector<int> *cur_n,*nex_n;
+        vector<int> *cur_e,*nex_e;
+        vector<int> n_a,n_b;
+        vector<int> e_a,e_b;
+        cur_n = &n_a;
+        nex_n = &n_b;
+        cur_e = &e_a;
+        nex_e = &e_b;
+        for(int i=0;i<node_num;i++) {
+            if(n_deg[i] < 0) continue;
+            mp_node[i] = n_deg[i];
+            cur_n->push_back(i);
+        }
+        for(int i=0;i<edge_num;i++) {
+            if(e_deg[i] < 0) continue;
+            mp_edge[i] = e_deg[i];
+            cur_e->push_back(i);
+        }
+
+        int k = 0;
+        while(true){
+            vector<int> del_node;
+            vector<int> del_edge;
+            for(auto &n_id:*cur_n){
+                if(mp_node[n_id]<=k) del_node.push_back(n_id);
+                else nex_n->push_back(n_id);
+            }
+            for(auto &e_id:*cur_e){
+                if(mp_edge[e_id]<=k) del_edge.push_back(e_id);
+                else nex_e->push_back(e_id);
+            }
+            // cerr<<"k:"<<k<<" rest:"<<nex_n->size()<<endl;
+            if(nex_n->size() == 0) return del_node;
+            
+            for(auto &n_id:del_node){
+                for(auto &e_id:Node[n_id].edges){
+                    // cerr<<mp_edge[e_id]<<endl;
+                    // assert(mp_edge[e_id] > 0);
+                    mp_edge[e_id] -= 1;
+                }
+            }
+
+            for(auto &e_id:del_edge){
+                for(auto &n_id:Edge[e_id].nodes){
+                    // assert(mp_node[n_id] > 0);
+                    mp_node[n_id] -= 1;
+                }
+            }
+
+            swap(cur_n,nex_n);
+            swap(cur_e,nex_e);
+            nex_n->clear();
+            nex_e->clear();
+            k+=1 ;
+        }
+        return ret;
+    }
+
+    void del_node(vector<int> del_nodes){
+        for(auto &n_id:del_nodes){
+            for(auto &e_id:Node[n_id].edges){
+                e_deg[e_id] -= 1;
+            }
+            n_deg[n_id] = -1;
+        }
+    }
+    void del_node(int del_nodes){
+        for(auto &e_id:Node[del_nodes].edges){
+            e_deg[e_id] -= 1;
+        }
+        n_deg[del_nodes] = -1;
+    }
+};
 void load_data(string path){
     unordered_map<int,HyperNode> mp_node;
     FILE *file;
@@ -76,14 +166,24 @@ void load_data(string path){
     }
 }
 
-void solve(int p,int topk){
-    int maxi_cap = n/p + 5;
+void solve(int p,int topk, int buffer_fac = 2){
+    int maxi_cap = n/p + 1;
     vector<set<int> > part_node;
     vector<set<int> > part_edge;
     map<int,int> eval;
     map<int,bool> assign_n; 
-    Heap heap(topk);
-    int heap_size = 1;
+    int heap_size = topk*buffer_fac;
+    Heap heap(heap_size);
+    K_core kcore(Node,Edge,n,m);
+    vector<int> a = kcore.get_kcore();
+    // cerr<<a.size()<<endl;
+    // kcore.del_node(a);
+    // a = kcore.get_kcore();
+    // cerr<<a.size()<<endl;
+    // for(int i=0;i<a.size();i++) {
+    //     cerr<<"test:"<<a[i]<<endl;
+    // }
+
 
     int cnt = 0;
     int cur_p = 0;
@@ -91,17 +191,27 @@ void solve(int p,int topk){
     part_edge.push_back(set<int>());
     clock_t beg_time = clock();
     while(cnt < n){
-        cnt += 1;
-        // if(cnt%100 == 0) cerr<<cnt<<endl;
-        for(int id=0;id<n;id++) {
-            if(assign_n[id] == true) continue;
-            if(Node[id].degree == 0) continue;
-            heap.add(eval[id],id);
+        vector<int> add_node;
+        if(part_node[cur_p].size()==0){
+            add_node = kcore.get_kcore();
+            cerr<<add_node.size()<<endl;
+        }else{
+            if(heap.size()<heap_size/2){
+                heap.clear();
+                for(int id=0;id<n;id++) {
+                    if(assign_n[id] == true) continue;
+                    // if(Node[id].degree == 0) continue;
+                    heap.add(eval[id],id);
+                }
+            }
+            int k = min(topk,heap.size());
+            for(int i=0;i<k;i++) add_node.push_back(heap.pop());
         }
-        while(heap.size()!=0){
-            int cur_node = heap.top();
-            heap.pop();
+        
+        for(auto &cur_node:add_node){
             assign_n[cur_node] = true;
+            cnt += 1;   
+            kcore.del_node(cur_node);
             part_node[cur_p].insert(cur_node);
             for(auto &e_id:Node[cur_node].edges){
                 if(part_edge[cur_p].find(e_id) == part_edge[cur_p].end()){
@@ -117,8 +227,17 @@ void solve(int p,int topk){
                 part_node.push_back(set<int>());
                 part_edge.push_back(set<int>());
                 eval.clear();
+                heap.clear();
+                break;
             }
         }
+        vector<int> rebuild;
+        while(heap.size()!=0) rebuild.push_back(heap.pop());
+        for(auto &n_id:rebuild){
+            heap.add(eval[n_id],n_id);
+        }
+        
+
     }
     clock_t end_time = clock();
     int k_1 = 0;
@@ -128,17 +247,17 @@ void solve(int p,int topk){
         k_1 += part_edge[i].size();
         for(auto &e_id:part_edge[i]) edge_set.insert(e_id);
     }
-    cerr<<"para:"<<endl<<"p:"<<p<<" topk:"<<topk<<endl;
+    cerr<<"para:"<<endl<<"p:"<<p<<" topk:"<<topk<<" buffer_fac:"<<buffer_fac<<endl;
     cerr<<"k-1: "<<k_1-edge_set.size()<<" runtime:"<<(end_time-beg_time)*1000/CLOCKS_PER_SEC<<"(ms)"<<endl<<"----------"<<endl;
 }
 int main(){
-    n = 4600;
-    m = 4600;
-    string path = "../data/wiki/wiki.txt";
+    // n = 4600;
+    // m = 4600;
+    // string path = "../data/wiki/wiki.txt";
 
-    // // // n = 56519;
-    // // // m = 120867;
-    // // // string path = "../data/github/github.txt";
+    n = 56520;
+    m = 120870;
+    string path = "../data/github/github.txt";
 
     Node = new HyperNode[n];
     Edge = new HyperEdge[m];
@@ -146,8 +265,10 @@ int main(){
     for(int i=0;i<m;i++) Edge[i].id = i;
 
     int p = 16;
+    int topk = 10;
+    int buffer_fac = 2;
     load_data(path);
-    solve(p,1);
+    solve(p,10,10);
     printf("OK");
 
     // Heap test(5);

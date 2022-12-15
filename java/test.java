b701db83b6c50bf81c4df3863f111930c8f1816c
guan_manjaro <1057368050@qq.com>
2022-12-15 09:59:48 +0800

    save before big change

--- java/test.java
+++ java/test.java
@@ -1,7 +1,7 @@
-import org.apache.spark._
-import org.apache.spark.graphx._
+import org.apache.spark._;
+import org.apache.spark.graphx._;
 // To make some of the examples work we will also need RDD
-import org.apache.spark.rdd.RDD
+import org.apache.spark.rdd.RDD;
 public class test {
     public static void main(String[] args){
       System.out.println("Hello vscode!");
